066560bc92cf0f2f9a29aa0787bc39e078b9f319
uduak <lorduduak@gmail.com>
2022-06-22 21:04:49 +0100

    infinite slideshow

---
+++ main.js
@@ -0,0 +1,45 @@
+const slideContainer = document.querySelector('.container');
+const slide = document.querySelector('.slides');
+const nextBtn = document.getElementById('next-btn');
+const prevBtn = document.getElementById('prev-btn');
+
+let slides = document.querySelectorAll('.slide');
+let index = 1;
+
+// clone first slide
+const firstClone = slides[0].cloneNode(true);
+
+// clone last slide
+const lastClone = slides[slides.length - 1].cloneNode(true);
+
+firstClone.id = 'first-clone';
+lastClone.id = 'last-clone';
+
+slide.append(firstClone);
+slide.prepend(lastClone);
+
+// get width of single slide
+const slideWidth = slides[index].clientWidth;
+
+// define sideshow. Since index is 1, first slide (clone of last is hidden)
+slide.style.transform = `translateX(${-slideWidth * index}px)`;
+
+// slideshow func
+const startSlide = () => {
+    setInterval(()=>{
+        index++;
+        slide.style.transform = `translateX(${-slideWidth * index}px)`;
+        slide.style.transition = '.7s';
+    }, 3000)
+}
+
+slide.addEventListener('transitionend', () => {
+    slides = document.querySelectorAll('.slide');
+    if(slides[index].id === firstClone.id) {
+        slide.style.transition = 'none';
+        index = 1;
+        slide.style.transform = `translateX(${-slideWidth * index}px)`;
+    }
+})
+
+startSlide();
